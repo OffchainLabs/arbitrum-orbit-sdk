7215aafeff4da2dab3f9de439378e7387ddc54df
Dragiša Spasojević <dragisaspasojevic@gmail.com>
2023-11-16 21:57:01 +0100

    feat(example): update with node config

--- example/index.ts
+++ example/index.ts
@@ -1,18 +1,15 @@
-import { createPublicClient, http } from 'viem';
+import { Chain, createPublicClient, http } from 'viem';
 import { generatePrivateKey, privateKeyToAccount } from 'viem/accounts';
 import { arbitrumGoerli } from 'viem/chains';
-
 import {
   createRollupPrepareConfig,
   createRollupPrepareChainConfig,
   createRollupPrepareTransactionRequest,
   createRollupPrepareTransactionReceipt,
+  prepareNodeConfig,
 } from '@arbitrum/orbit-sdk';
 import { generateChainId } from '@arbitrum/orbit-sdk/utils';
-
-if (typeof process.env.PRIVATE_KEY === 'undefined') {
-  throw new Error(`Please provide the "PRIVATE_KEY" environment variable`);
-}
+import { writeFile } from 'fs/promises';
 
 function sanitizePrivateKey(privateKey: string): `0x${string}` {
   if (!privateKey.startsWith('0x')) {
@@ -22,55 +19,95 @@ function sanitizePrivateKey(privateKey: string): `0x${string}` {
   return privateKey as `0x${string}`;
 }
 
-const chain = arbitrumGoerli;
+function withFallbackPrivateKey(privateKey: string | undefined): `0x${string}` {
+  if (typeof privateKey === 'undefined') {
+    return generatePrivateKey();
+  }
 
-const batchPoster = privateKeyToAccount(generatePrivateKey()).address;
-const validator = privateKeyToAccount(generatePrivateKey()).address;
+  return sanitizePrivateKey(privateKey);
+}
 
-const publicClient = createPublicClient({
-  chain,
-  transport: http(),
-});
+function getRpcUrl(chain: Chain) {
+  return chain.rpcUrls.default.http[0];
+}
 
-const account = privateKeyToAccount(
-  sanitizePrivateKey(process.env.PRIVATE_KEY)
-);
+function getBlockExplorerUrl(chain: Chain) {
+  return chain.blockExplorers?.default.url;
+}
+
+if (typeof process.env.DEPLOYER_PRIVATE_KEY === 'undefined') {
+  throw new Error(`Please provide the "DEPLOYER_PRIVATE_KEY" environment variable`);
+}
+
+// load or generate a random batch poster account
+const batchPosterPrivateKey = withFallbackPrivateKey(process.env.BATCH_POSTER_PRIVATE_KEY);
+const batchPoster = privateKeyToAccount(batchPosterPrivateKey).address;
+
+// load or generate a random validator account
+const validatorPrivateKey = withFallbackPrivateKey(process.env.VALIDATOR_PRIVATE_KEY);
+const validator = privateKeyToAccount(validatorPrivateKey).address;
+
+// set the parent chain and create a public client for it
+const parentChain = arbitrumGoerli;
+const parentChainPublicClient = createPublicClient({ chain: parentChain, transport: http() });
+
+// load the deployer account
+const deployer = privateKeyToAccount(sanitizePrivateKey(process.env.DEPLOYER_PRIVATE_KEY));
 
 async function main() {
+  // generate a random chain id
   const chainId = generateChainId();
 
+  // create the chain config
+  const chainConfig = createRollupPrepareChainConfig({
+    chainId,
+    arbitrum: { InitialChainOwner: deployer.address, DataAvailabilityCommittee: true },
+  });
+
+  // prepare the transaction for deploying the core contracts
   const request = await createRollupPrepareTransactionRequest({
     params: {
       config: createRollupPrepareConfig({
         chainId: BigInt(chainId),
-        owner: account.address,
-        chainConfig: createRollupPrepareChainConfig({
-          chainId,
-          arbitrum: {
-            InitialChainOwner: account.address,
-            DataAvailabilityCommittee: true,
-          },
-        }),
+        owner: deployer.address,
+        chainConfig,
       }),
       batchPoster,
       validators: [validator],
     },
-    account: account.address,
-    publicClient,
+    account: deployer.address,
+    publicClient: parentChainPublicClient,
   });
 
-  const txHash = await publicClient.sendRawTransaction({
-    serializedTransaction: await account.signTransaction(request),
+  // sign and send the transaction
+  const txHash = await parentChainPublicClient.sendRawTransaction({
+    serializedTransaction: await deployer.signTransaction(request),
   });
 
+  // get the transaction receipt after waiting for the transaction to complete
   const txReceipt = createRollupPrepareTransactionReceipt(
-    await publicClient.waitForTransactionReceipt({ hash: txHash })
+    await parentChainPublicClient.waitForTransactionReceipt({ hash: txHash })
   );
 
-  console.log(
-    `Contracts deployed in transaction: ${chain.blockExplorers.default.url}/tx/${txReceipt.transactionHash}`
-  );
-  console.dir(txReceipt.getCoreContracts());
+  // get information about the deployed core contracts from the transaction receipt
+  const coreContracts = txReceipt.getCoreContracts();
+  const coreContractsDeploymentBlockNumber = Number(txReceipt.blockNumber);
+  console.log(`Deployed in ${getBlockExplorerUrl(parentChain)}/tx/${txReceipt.transactionHash}`);
+
+  // prepare the node config
+  const nodeConfig = prepareNodeConfig({
+    chainName: 'My Orbit Chain',
+    chainConfig,
+    coreContracts,
+    coreContractsDeploymentBlockNumber,
+    batchPosterPrivateKey,
+    validatorPrivateKey,
+    parentChainId: parentChain.id,
+    parentChainRpcUrl: getRpcUrl(parentChain),
+  });
+
+  await writeFile('node-config.json', JSON.stringify(nodeConfig, null, 2));
+  console.log(`Node config written to "node-config.json"`);
 }
 
 main();
