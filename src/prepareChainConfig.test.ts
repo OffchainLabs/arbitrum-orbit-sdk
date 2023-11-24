6556cdd8297da1089eb4b3258a57e52a8947d181
Dragiša Spasojević <dragisaspasojevic@gmail.com>
2023-11-24 21:44:15 +0100

    test: switch to snapshot test for prepare chain config

--- src/prepareChainConfig.test.ts
+++ src/prepareChainConfig.test.ts
@@ -1,40 +1,10 @@
 import { it, expect } from 'vitest';
 
 import { ChainConfig } from './types/ChainConfig';
-import { defaults, prepareChainConfig } from './prepareChainConfig';
+import { prepareChainConfig } from './prepareChainConfig';
 
 const chainId = 69_420;
 const vitalik: `0x${string}` = '0xd8da6bf26964af9d7eed9e03e53415d37aa96045';
-
-function assertChainConfigsEqual(aConfig: ChainConfig, bConfig: ChainConfig) {
-  expect(aConfig.chainId).toEqual(bConfig.chainId);
-  expect(aConfig.homesteadBlock).toEqual(bConfig.homesteadBlock);
-  expect(aConfig.daoForkBlock).toEqual(bConfig.daoForkBlock);
-  expect(aConfig.daoForkSupport).toEqual(bConfig.daoForkSupport);
-  expect(aConfig.eip150Block).toEqual(bConfig.eip150Block);
-  expect(aConfig.eip150Hash).toEqual(bConfig.eip150Hash);
-  expect(aConfig.eip155Block).toEqual(bConfig.eip155Block);
-  expect(aConfig.eip158Block).toEqual(bConfig.eip158Block);
-  expect(aConfig.byzantiumBlock).toEqual(bConfig.byzantiumBlock);
-  expect(aConfig.constantinopleBlock).toEqual(bConfig.constantinopleBlock);
-  expect(aConfig.petersburgBlock).toEqual(bConfig.petersburgBlock);
-  expect(aConfig.istanbulBlock).toEqual(bConfig.istanbulBlock);
-  expect(aConfig.muirGlacierBlock).toEqual(bConfig.muirGlacierBlock);
-  expect(aConfig.berlinBlock).toEqual(bConfig.berlinBlock);
-  expect(aConfig.londonBlock).toEqual(bConfig.londonBlock);
-  expect(aConfig.clique.period).toEqual(bConfig.clique.period);
-  expect(aConfig.clique.epoch).toEqual(bConfig.clique.epoch);
-
-  const { arbitrum: aConfigArb } = aConfig;
-  const { arbitrum: bConfigArb } = bConfig;
-
-  expect(aConfigArb.EnableArbOS).toEqual(bConfigArb.EnableArbOS);
-  expect(aConfigArb.AllowDebugPrecompiles).toEqual(bConfigArb.AllowDebugPrecompiles);
-  expect(aConfigArb.DataAvailabilityCommittee).toEqual(bConfigArb.DataAvailabilityCommittee);
-  expect(aConfigArb.InitialArbOSVersion).toEqual(bConfigArb.InitialArbOSVersion);
-  expect(aConfigArb.InitialChainOwner).toEqual(bConfigArb.InitialChainOwner);
-  expect(aConfigArb.GenesisBlockNum).toEqual(bConfigArb.GenesisBlockNum);
-}
 
 it('creates chain config with defaults', () => {
   const params = {
@@ -44,16 +14,7 @@ it('creates chain config with defaults', () => {
     },
   };
 
-  const result = prepareChainConfig(params);
-
-  assertChainConfigsEqual(result, {
-    ...defaults,
-    chainId,
-    arbitrum: {
-      ...defaults.arbitrum,
-      InitialChainOwner: vitalik,
-    },
-  });
+  expect(prepareChainConfig(params)).toMatchSnapshot();
 });
 
 it('creates chain config with custom params', () => {
@@ -89,7 +50,5 @@ it('creates chain config with custom params', () => {
     },
   };
 
-  const result = prepareChainConfig(params);
-
-  assertChainConfigsEqual(result, params);
+  expect(prepareChainConfig(params)).toMatchSnapshot();
 });
