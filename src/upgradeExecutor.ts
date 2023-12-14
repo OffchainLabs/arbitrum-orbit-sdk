60ad267d1005547ded24348ad6a1cdddf451d8cd
Dragiša Spasojević <dragisaspasojevic@gmail.com>
2023-12-14 12:20:22 +0100

    types

--- src/upgradeExecutor.ts
+++ src/upgradeExecutor.ts
@@ -1,20 +1,24 @@
-import { encodeFunctionData, GetFunctionArgs } from 'viem';
+import { encodeFunctionData, EncodeFunctionDataParameters } from 'viem';
 
 import { upgradeExecutor } from './contracts';
-import { GetFunctionName } from './types/utils';
+import { GetFunctionName, Prettify } from './types/utils';
 
 export type UpgradeExecutorAbi = typeof upgradeExecutor.abi;
+
 export type UpgradeExecutorFunctionName = GetFunctionName<UpgradeExecutorAbi>;
+
+export type UpgradeExecutorEncodeFunctionDataParameters<
+  TFunctionName extends UpgradeExecutorFunctionName
+> = Prettify<
+  Omit<EncodeFunctionDataParameters<UpgradeExecutorAbi, TFunctionName>, 'abi'>
+>;
 
 export function upgradeExecutorEncodeFunctionData<
   TFunctionName extends UpgradeExecutorFunctionName
 >({
   functionName,
   args,
-}: { functionName: TFunctionName } & GetFunctionArgs<
-  UpgradeExecutorAbi,
-  TFunctionName
->) {
+}: UpgradeExecutorEncodeFunctionDataParameters<TFunctionName>) {
   // todo: fix this weird type issue
   //
   // @ts-ignore
