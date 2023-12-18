6ad28f5d4fffb51cd7bdb139ea08a01023760185
Dragiša Spasojević <dragisaspasojevic@gmail.com>
2023-12-18 23:58:42 +0100

    switch to local for test

--- src/arbOwnerReadContract.test.ts
+++ src/arbOwnerReadContract.test.ts
@@ -1,33 +1,33 @@
 import { it, expect } from 'vitest';
 import { createPublicClient, http } from 'viem';
-import { arbitrumSepolia } from 'viem/chains';
 
+import { arbitrumLocal } from './testHelpers';
 import { arbOwnerReadContract } from './arbOwnerReadContract';
 
-const arbitrumSepoliaClient = createPublicClient({
-  chain: arbitrumSepolia,
+const client = createPublicClient({
+  chain: arbitrumLocal,
   transport: http(),
 });
 
 it('correctly fetches network fee account for arbitrum sepolia', async () => {
-  const result = await arbOwnerReadContract(arbitrumSepoliaClient, {
+  const result = await arbOwnerReadContract(client, {
     functionName: 'getNetworkFeeAccount',
   });
 
-  expect(result).toEqual('0x71B61c2E250AFa05dFc36304D6c91501bE0965D8');
+  expect(result).toEqual('0x3f1Eae7D46d88F08fc2F8ed27FCb2AB183EB2d0E');
 });
 
 it('correctly fetches chain owners for arbitrum sepolia', async () => {
-  const result = await arbOwnerReadContract(arbitrumSepoliaClient, {
+  const result = await arbOwnerReadContract(client, {
     functionName: 'getAllChainOwners',
   });
 
   expect(result).toHaveLength(1);
-  expect(result[0]).toEqual('0x71B61c2E250AFa05dFc36304D6c91501bE0965D8');
+  expect(result[0]).toEqual('0x3f1Eae7D46d88F08fc2F8ed27FCb2AB183EB2d0E');
 });
 
 it('correctly checks invalid chain owner for arbitrum sepolia', async () => {
-  const result = await arbOwnerReadContract(arbitrumSepoliaClient, {
+  const result = await arbOwnerReadContract(client, {
     functionName: 'isChainOwner',
     args: ['0x31421C442c422BD16aef6ae44D3b11F404eeaBd9'],
   });
@@ -36,9 +36,9 @@ it('correctly checks invalid chain owner for arbitrum sepolia', async () => {
 });
 
 it('correctly checks valid chain owner for arbitrum sepolia', async () => {
-  const result = await arbOwnerReadContract(arbitrumSepoliaClient, {
+  const result = await arbOwnerReadContract(client, {
     functionName: 'isChainOwner',
-    args: ['0x71B61c2E250AFa05dFc36304D6c91501bE0965D8'],
+    args: ['0x3f1Eae7D46d88F08fc2F8ed27FCb2AB183EB2d0E'],
   });
 
   expect(result).toEqual(true);
