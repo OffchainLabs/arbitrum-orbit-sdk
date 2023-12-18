cb340918dad265991fc3ccb51ab39521b6f5e5b8
spsjvc <git@spsjvc.com>
2023-12-19 00:56:59 +0100

    clean up

--- src/types/utils.ts
+++ src/types/utils.ts
@@ -1,5 +1,4 @@
 import { Abi } from 'viem';
-import { arbOwner } from '../contracts';
 
 // https://twitter.com/mattpocockuk/status/1622730173446557697
 export type Prettify<T> = {
@@ -10,16 +9,3 @@ export type GetFunctionName<TAbi extends Abi> = Extract<
   TAbi[number],
   { type: 'function' }
 >['name'];
-
-export type ExtractReadFunctionsFromAbi<TAbi extends Abi> = Extract<
-  TAbi[number],
-  { type: 'function'; stateMutability: 'view' | 'pure' }
->[];
-
-export type ExtractWriteFunctionsFromAbi<TAbi extends Abi> = Extract<
-  TAbi[number],
-  { type: 'function'; stateMutability: 'nonpayable' | 'payable' }
->[];
-
-type X = GetFunctionName<ExtractReadFunctionsFromAbi<typeof arbOwner.abi>>;
-type Y = GetFunctionName<ExtractWriteFunctionsFromAbi<typeof arbOwner.abi>>;
