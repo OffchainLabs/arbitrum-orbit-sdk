f2c2f2db1b5922a44425434b6fca3ef507ffe4c0
spsjvc <git@spsjvc.com>
2023-12-20 15:09:07 +0100

    format

--- src/chains.ts
+++ src/chains.ts
@@ -14,7 +14,6 @@ const nitroTestnodeL1 = defineChain({
       http: ['http://127.0.0.1:8545'],
     },
   },
-
   testnet: true,
 });
 
@@ -34,5 +33,11 @@ const nitroTestnodeL2 = defineChain({
   testnet: true,
 });
 
-export const chains = [sepolia, arbitrumSepolia, nitroTestnodeL1, nitroTestnodeL2] as const;
+export const chains = [
+  sepolia,
+  arbitrumSepolia,
+  nitroTestnodeL1,
+  nitroTestnodeL2,
+] as const;
+
 export { sepolia, arbitrumSepolia, nitroTestnodeL1, nitroTestnodeL2 };
