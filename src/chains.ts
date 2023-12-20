9199d0da3a4b0e91391c25baf60a0db13a9d967d
spsjvc <git@spsjvc.com>
2023-12-20 18:29:21 +0100

    feat: add support for nitro-testnode (#21)

--- src/chains.ts
+++ src/chains.ts
@@ -1,4 +1,43 @@
+import { defineChain } from 'viem';
 import { sepolia, arbitrumSepolia } from 'viem/chains';
 
-export const chains = [sepolia, arbitrumSepolia];
-export { sepolia, arbitrumSepolia };
+const nitroTestnodeL1 = defineChain({
+  id: 1_337,
+  network: 'Nitro Testnode L1',
+  name: 'nitro-testnode-l1',
+  nativeCurrency: { name: 'Ether', symbol: 'ETH', decimals: 18 },
+  rpcUrls: {
+    default: {
+      http: ['http://127.0.0.1:8545'],
+    },
+    public: {
+      http: ['http://127.0.0.1:8545'],
+    },
+  },
+  testnet: true,
+});
+
+const nitroTestnodeL2 = defineChain({
+  id: 412_346,
+  network: 'Nitro Testnode L2',
+  name: 'nitro-testnode-l2',
+  nativeCurrency: { name: 'Ether', symbol: 'ETH', decimals: 18 },
+  rpcUrls: {
+    default: {
+      http: ['http://127.0.0.1:8547'],
+    },
+    public: {
+      http: ['http://127.0.0.1:8547'],
+    },
+  },
+  testnet: true,
+});
+
+export const chains = [
+  sepolia,
+  arbitrumSepolia,
+  nitroTestnodeL1,
+  nitroTestnodeL2,
+] as const;
+
+export { sepolia, arbitrumSepolia, nitroTestnodeL1, nitroTestnodeL2 };
