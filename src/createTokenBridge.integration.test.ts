e7d0f69aa7a2ad9b552bf54a2c9cc77ea01a1fe9
Dragiša Spasojević <dragisaspasojevic@gmail.com>
2024-01-22 15:03:16 +0100

    fix test

--- src/createTokenBridge.integration.test.ts
+++ src/createTokenBridge.integration.test.ts
@@ -18,14 +18,14 @@ const nitroTestnodeL2Client = createPublicClient({
   transport: http(nitroTestnodeL2.rpcUrls.default.http[0]),
 });
 
-it(`successfully deploys token bridge contracts on parent chain`, async () => {
-  const createRollupTxReceipt = await testSetupCreateRollup({
-    publicClient: nitroTestnodeL1Client,
-  });
-  const { rollup } = createRollupTxReceipt.getCoreContracts();
-
+it(`successfully deploys token bridge contracts through token bridge creator`, async () => {
   const txRequest = await createTokenBridgePrepareTransactionRequest({
-    params: { rollup, rollupOwner: deployer.address },
+    params: {
+      rollup: '0x05e720d41d78ad9e43cd599e2fbf924dac867124',
+      // https://github.com/OffchainLabs/nitro-testnode/blob/master/test-node.bash#L363
+      // https://github.com/OffchainLabs/nitro-testnode/blob/master/scripts/accounts.ts#L35
+      rollupOwner: '0xe2148eE53c0755215Df69b2616E552154EdC584f',
+    },
     parentChainPublicClient: nitroTestnodeL1Client,
     childChainPublicClient: nitroTestnodeL2Client,
     account: deployer.address,
@@ -42,12 +42,13 @@ it(`successfully deploys token bridge contracts on parent chain`, async () => {
   );
 
   expect(txReceipt.status).toEqual('success');
-  expect(async () => {
+
+  await expect(async () => {
     const retryables = await txReceipt.waitForRetryables({
       orbitPublicClient: nitroTestnodeL2Client,
     });
 
     console.log(`Retryable 1: ${retryables[0].transactionHash}`);
     console.log(`Retryable 2: ${retryables[1].transactionHash}`);
-  }).not.toThrow();
+  }).rejects.not.toThrowError();
 });
