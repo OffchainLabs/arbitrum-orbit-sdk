d462cdeeabd47ca28863eefbfb6923e3ee6b3bc9
Dragiša Spasojević <dragisaspasojevic@gmail.com>
2024-01-23 11:07:52 +0100

    remove unneeded code

--- src/testHelpers.ts
+++ src/testHelpers.ts
@@ -1,15 +1,5 @@
-import { PublicClient } from 'viem';
 import { privateKeyToAccount, PrivateKeyAccount } from 'viem/accounts';
 import { config } from 'dotenv';
-
-import {
-  CreateRollupTransactionReceipt,
-  createRollupPrepareTransactionReceipt,
-} from './createRollupPrepareTransactionReceipt';
-import { generateChainId } from './utils';
-import { prepareChainConfig } from './prepareChainConfig';
-import { createRollupPrepareConfig } from './createRollupPrepareConfig';
-import { createRollupPrepareTransactionRequest } from './createRollupPrepareTransactionRequest';
 
 config();
 
@@ -31,50 +21,4 @@ function sanitizePrivateKey(privateKey: string): `0x${string}` {
   }
 
   return privateKey as `0x${string}`;
-}
-
-export type TestSetupCreateRollupParameters = {
-  publicClient: PublicClient;
-};
-
-export async function testSetupCreateRollup({
-  publicClient,
-}: TestSetupCreateRollupParameters): Promise<CreateRollupTransactionReceipt> {
-  const deployer = getTestPrivateKeyAccount();
-
-  // generate a random chain id
-  const chainId = generateChainId();
-
-  // create the chain config
-  const chainConfig = prepareChainConfig({
-    chainId,
-    arbitrum: { InitialChainOwner: deployer.address },
-  });
-
-  const config = createRollupPrepareConfig({
-    chainId: BigInt(chainId),
-    owner: deployer.address,
-    chainConfig,
-  });
-
-  // prepare the transaction for deploying the core contracts
-  const request = await createRollupPrepareTransactionRequest({
-    params: {
-      config,
-      batchPoster: deployer.address,
-      validators: [deployer.address],
-    },
-    account: deployer.address,
-    publicClient,
-  });
-
-  // sign and send the transaction
-  const txHash = await publicClient.sendRawTransaction({
-    serializedTransaction: await deployer.signTransaction(request),
-  });
-
-  // get the transaction receipt after waiting for the transaction to complete
-  return createRollupPrepareTransactionReceipt(
-    await publicClient.waitForTransactionReceipt({ hash: txHash }),
-  );
 }
