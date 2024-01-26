cb9d56434dd1c99904ea370c47bf79dd0897e47d
TucksonDev <tucksondev@gmail.com>
2024-01-26 09:20:09 +0000

    Increase tests timeout

--- vitest.config.ts
+++ vitest.config.ts
@@ -3,8 +3,8 @@ import { defineConfig } from 'vitest/config';
 export default defineConfig({
   test: {
     globals: true,
-    // allow tests to run for 5 minutes as retryables can take a while
-    testTimeout: 300_000,
+    // allow tests to run for 7 minutes as retryables can take a while
+    testTimeout: 7 * 60 * 1000,
     // don't run tests in parallel to avoid race conditions
     sequence: { concurrent: false },
   },
