3dac3f0e2709d571a2c00fe648fc3c53fbaf479b
Tuckson <105675159+TucksonDev@users.noreply.github.com>
2024-02-13 10:05:52 +0000

    feat: update node config for nitro v2.2.x (#45)

--- src/prepareNodeConfig.ts
+++ src/prepareNodeConfig.ts
@@ -102,17 +102,10 @@ export function prepareNodeConfig({
       api: ['eth', 'net', 'web3', 'arb', 'debug'],
     },
     'node': {
-      'forwarding-target': '',
-      'sequencer': {
-        'max-tx-data-size': 85000,
-        'enable': true,
-        'dangerous': {
-          'no-coordinator': true,
-        },
-        'max-block-speed': '250ms',
-      },
+      'sequencer': true,
       'delayed-sequencer': {
-        enable: true,
+        'enable': true,
+        'use-merge-finality': false,
       },
       'batch-poster': {
         'max-size': 90000,
@@ -128,6 +121,17 @@ export function prepareNodeConfig({
           'private-key': sanitizePrivateKey(validatorPrivateKey),
         },
       },
+      'dangerous': {
+        'no-sequencer-coordinator': true,
+      },
+    },
+    'execution': {
+      'forwarding-target': '',
+      'sequencer': {
+        'enable': true,
+        'max-tx-data-size': 85000,
+        'max-block-speed': '250ms',
+      },
       'caching': {
         archive: true,
       },
@@ -141,7 +145,7 @@ export function prepareNodeConfig({
       'parent-chain-node-url': parentChainRpcUrl,
       'rest-aggregator': {
         enable: true,
-        urls: 'http://localhost:9876',
+        urls: 'http://localhost:9877',
       },
       'rpc-aggregator': {
         'enable': true,
