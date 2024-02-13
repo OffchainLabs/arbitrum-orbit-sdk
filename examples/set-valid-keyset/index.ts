52ef2e1e5d4b1e3d11d3885c2e3c2f5d76e7db25
Dragiša Spasojević <dragisaspasojevic@gmail.com>
2024-02-13 10:55:33 +0100

    feat: sanitize private key

--- examples/set-valid-keyset/index.ts
+++ examples/set-valid-keyset/index.ts
@@ -2,14 +2,7 @@ import { Chain, createPublicClient, http } from 'viem';
 import { privateKeyToAccount } from 'viem/accounts';
 import { arbitrumSepolia } from 'viem/chains';
 import { setValidKeysetPrepareTransactionRequest } from '@arbitrum/orbit-sdk';
-
-function sanitizePrivateKey(privateKey: string): `0x${string}` {
-  if (!privateKey.startsWith('0x')) {
-    return `0x${privateKey}`;
-  }
-
-  return privateKey as `0x${string}`;
-}
+import { sanitizePrivateKey } from '@arbitrum/orbit-sdk/utils';
 
 function getBlockExplorerUrl(chain: Chain) {
   return chain.blockExplorers?.default.url;
