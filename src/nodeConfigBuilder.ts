96dcf9df4c3eb06f15dfa278e810fe794964adf7
spsjvc <git@spsjvc.com>
2024-02-23 18:07:59 +0100

    add defaults

--- src/nodeConfigBuilder.ts
+++ src/nodeConfigBuilder.ts
@@ -16,7 +16,11 @@ type NodeConfigKey = NodeConfigParams['key'];
 type NodeConfigValue<TKey extends NodeConfigKey> = Extract<NodeConfigParams, { key: TKey }>['type'];
 
 class NodeConfigBuilder {
-  private nodeConfig: NodeConfig = {};
+  private nodeConfig: NodeConfig;
+
+  constructor(initialNodeConfig?: NodeConfig) {
+    this.nodeConfig = initialNodeConfig ?? {};
+  }
 
   set<TKey extends NodeConfigKey>(key: TKey, value: NodeConfigValue<TKey>): NodeConfigBuilder {
     throw new Error(`not yet implemented`);
@@ -27,8 +31,18 @@ class NodeConfigBuilder {
   }
 }
 
-export function createNodeConfigBuilder(): NodeConfigBuilder {
-  return new NodeConfigBuilder();
+type CreateNodeConfigBuilderParams = {
+  withoutDefaults: boolean;
+};
+
+export function createNodeConfigBuilder(params?: CreateNodeConfigBuilderParams): NodeConfigBuilder {
+  const withoutDefaults = params?.withoutDefaults ?? false;
+
+  if (withoutDefaults) {
+    return new NodeConfigBuilder();
+  }
+
+  return new NodeConfigBuilder(nodeConfigDefaults);
 }
 
 const nodeConfig = createNodeConfigBuilder()
@@ -36,3 +50,43 @@ const nodeConfig = createNodeConfigBuilder()
   .set('auth.addr', '127.0.0.1')
   .set('auth.api', ['eth', 'net', 'web3', 'arb', 'debug'])
   .build();
+
+const nodeConfigDefaults: NodeConfig = {
+  http: {
+    addr: '0.0.0.0',
+    port: 8449,
+    vhosts: ['*'],
+    corsdomain: ['*'],
+    api: ['eth', 'net', 'web3', 'arb', 'debug'],
+  },
+  node: {
+    'sequencer': true,
+    'delayed-sequencer': {
+      'enable': true,
+      'use-merge-finality': false,
+      'finalize-distance': 1,
+    },
+    'batch-poster': {
+      'max-size': 90000,
+      'enable': true,
+    },
+    'staker': {
+      enable: true,
+      strategy: 'MakeNodes',
+    },
+    'dangerous': {
+      'no-sequencer-coordinator': true,
+    },
+  },
+  execution: {
+    'forwarding-target': '',
+    'sequencer': {
+      'enable': true,
+      'max-tx-data-size': 85000,
+      'max-block-speed': '250ms',
+    },
+    'caching': {
+      archive: true,
+    },
+  },
+};
