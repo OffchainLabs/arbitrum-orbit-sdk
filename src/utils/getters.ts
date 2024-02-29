da81754b21f0af3a5b4706e338bef164307985af
spsjvc <git@spsjvc.com>
2024-02-29 12:43:31 +0100

    update getters

--- src/utils/getters.ts
+++ src/utils/getters.ts
@@ -1,12 +1,24 @@
 import { PublicClient } from 'viem';
-import { rollupCreator } from '../contracts';
+
+import { rollupCreator, tokenBridgeCreator } from '../contracts';
 import { validateParentChainId } from '../types/ParentChain';
 
 export const getRollupCreatorAddress = (client: PublicClient) => {
   const chainId = validateParentChainId(client);
 
   if (!rollupCreator.address[chainId]) {
-    throw new Error(`Invalid chainId or chainId not supported: ${chainId}`);
+    throw new Error(`Parent chain not supported: ${chainId}`);
   }
+
   return rollupCreator.address[chainId];
+};
+
+export const getTokenBridgeCreatorAddress = (client: PublicClient) => {
+  const chainId = validateParentChainId(client);
+
+  if (!tokenBridgeCreator.address[chainId]) {
+    throw new Error(`Parent chain not supported: ${chainId}`);
+  }
+
+  return tokenBridgeCreator.address[chainId];
 };
