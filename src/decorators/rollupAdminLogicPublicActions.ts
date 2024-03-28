887601efc0c2e6bf134747177ba7d2dbb4a398c0
Fionna Chan <fionnachan@users.noreply.github.com>
2024-03-28 17:28:57 +0000

    skeleton

---
+++ src/decorators/rollupAdminLogicPublicActions.ts
@@ -0,0 +1,35 @@
+import { Transport, Chain, PrepareTransactionRequestReturnType, PublicClient } from 'viem';
+
+import {
+  rollupAdminLogicReadContract,
+  RollupAdminLogicFunctionName,
+  RollupAdminLogicReadContractParameters,
+  RollupAdminLogicReadContractReturnType,
+} from '../rollupAdminLogicReadContract';
+import {
+  rollupAdminLogicPrepareTransactionRequest,
+  ArbOwnerPrepareTransactionRequestParameters,
+} from '../rollupAdminLogicPrepareTransactionRequest';
+
+export type RollupAdminLogicActions<TChain extends Chain | undefined = Chain | undefined> = {
+  rollupAdminLogicReadContract: <TFunctionName extends RollupAdminLogicFunctionName>(
+    args: RollupAdminLogicReadContractParameters<TFunctionName>,
+  ) => Promise<RollupAdminLogicReadContractReturnType<TFunctionName>>;
+
+  rollupAdminLogicPrepareTransactionRequest: (
+    args: ArbOwnerPrepareTransactionRequestParameters,
+  ) => Promise<PrepareTransactionRequestReturnType<TChain>>;
+};
+
+export function rollupAdminLogicPublicActions<
+  TTransport extends Transport = Transport,
+  TChain extends Chain | undefined = Chain | undefined,
+>(client: PublicClient<TTransport, TChain>): RollupAdminLogicActions<TChain> {
+  return {
+    // @ts-ignore
+    rollupAdminLogicReadContract: (args) => rollupAdminLogicReadContract(client, args),
+
+    rollupAdminLogicPrepareTransactionRequest: (args) =>
+      rollupAdminLogicPrepareTransactionRequest(client, args),
+  };
+}
