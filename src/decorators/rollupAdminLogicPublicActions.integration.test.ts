6d14e6351825007d660ecc91334225aa38f2ecb5
Fionna Chan <13184582+fionnachan@users.noreply.github.com>
2024-04-02 18:17:40 +0100

    add integration test file

---
+++ src/decorators/rollupAdminLogicPublicActions.integration.test.ts
@@ -0,0 +1,36 @@
+import { it, expect } from 'vitest';
+import { PrepareTransactionRequestReturnType, createPublicClient, http } from 'viem';
+import { generatePrivateKey, privateKeyToAccount } from 'viem/accounts';
+
+import { nitroTestnodeL2 } from '../chains';
+import { rollupAdminLogicPublicActions } from './rollupAdminLogicPublicActions';
+import { getNitroTestnodePrivateKeyAccounts } from '../testHelpers';
+
+// l2 owner private key
+const devPrivateKey = getNitroTestnodePrivateKeyAccounts().l2RollupOwner.privateKey;
+
+const owner = privateKeyToAccount(devPrivateKey);
+const randomAccount = privateKeyToAccount(generatePrivateKey());
+
+const rollupAddress = '0x';
+
+const client = createPublicClient({
+  chain: nitroTestnodeL2,
+  transport: http(),
+}).extend(rollupAdminLogicPublicActions);
+
+it('successfully sets delayed inbox', async () => {
+  const transactionRequest = await client.rollupAdminLogicPrepareTransactionRequest({
+    functionName: 'setDelayedInbox',
+    args: ['0x', true],
+    rollupAdminLogicAddress: rollupAddress,
+    account: owner.address,
+  });
+
+  // submit tx to add chain owner
+  await client.sendRawTransaction({
+    serializedTransaction: await owner.signTransaction(
+      transactionRequest as PrepareTransactionRequestReturnType & { chainId: number },
+    ),
+  });
+});
--- src/rollupAdminLogicPrepareTransactionRequest.ts
+++ src/rollupAdminLogicPrepareTransactionRequest.ts
@@ -6,10 +6,10 @@ import {
   Chain,
   Transport,
   PrepareTransactionRequestParameters,
+  PrepareTransactionRequestReturnType,
 } from 'viem';
 
 import { rollupAdminLogicABI } from './contracts';
-import { upgradeExecutorEncodeFunctionData } from './upgradeExecutor';
 import { Prettify } from './types/utils';
 
 type RollupAdminLogicEncodeFunctionDataParameters = Prettify<
@@ -29,37 +29,18 @@ function rollupAdminLogicEncodeFunctionData({
   });
 }
 
-function rollupAdminLogicPrepareFunctionData(
-  params: RollupAdminLogicEncodeFunctionDataParameters & {
-    upgradeExecutor: Address | false;
-  },
-) {
-  const { upgradeExecutor, rollupAdminLogicAddress } = params;
-
-  if (!upgradeExecutor) {
-    return {
-      to: rollupAdminLogicAddress,
-      data: rollupAdminLogicEncodeFunctionData(params),
-      value: BigInt(0),
-    };
-  }
+function rollupAdminLogicPrepareFunctionData(params: RollupAdminLogicEncodeFunctionDataParameters) {
+  const { rollupAdminLogicAddress } = params;
 
   return {
-    to: upgradeExecutor,
-    data: upgradeExecutorEncodeFunctionData({
-      functionName: 'executeCall',
-      args: [
-        rollupAdminLogicAddress, // target
-        rollupAdminLogicEncodeFunctionData(params), // targetCallData
-      ],
-    }),
+    to: rollupAdminLogicAddress,
+    data: rollupAdminLogicEncodeFunctionData(params),
     value: BigInt(0),
   };
 }
 
 export type RollupAdminLogicPrepareTransactionRequestParameters = Prettify<
   RollupAdminLogicEncodeFunctionDataParameters & {
-    upgradeExecutor: Address | false;
     account: Address;
   }
 >;
@@ -67,7 +48,7 @@ export type RollupAdminLogicPrepareTransactionRequestParameters = Prettify<
 export async function rollupAdminLogicPrepareTransactionRequest<TChain extends Chain | undefined>(
   client: PublicClient<Transport, TChain>,
   params: RollupAdminLogicPrepareTransactionRequestParameters,
-) {
+): Promise<PrepareTransactionRequestReturnType<TChain> & { chainId: number }> {
   if (typeof client.chain === 'undefined') {
     throw new Error('[rollupAdminLogicPrepareTransactionRequest] client.chain is undefined');
   }
