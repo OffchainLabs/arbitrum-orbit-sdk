1cfffe419513b788c26393f14b9e0ba8980d16d1
Christophe <contact@christophedeveaux.com>
2024-04-05 18:57:18 +0200

    Fix registerNewNetworks for l2 networks

--- src/utils/registerNewNetwork.ts
+++ src/utils/registerNewNetwork.ts
@@ -5,38 +5,38 @@ import {
   addCustomNetwork,
   constants as arbitrumSdkConstants,
 } from '@arbitrum/sdk';
-import {
-  getL1Network,
-  getL2Network,
-  l1Networks,
-  l2Networks,
-} from '@arbitrum/sdk/dist/lib/dataEntities/networks';
+import { getL1Network, getL2Network } from '@arbitrum/sdk/dist/lib/dataEntities/networks';
 import { RollupAdminLogic__factory } from '@arbitrum/sdk/dist/lib/abi/factories/RollupAdminLogic__factory';
+import { isArbitrumChain } from '@arbitrum/sdk/dist/lib/utils/lib';
+
+async function isNetworkRegistered(provider: JsonRpcProvider, { type }: { type: 'L1' | 'L2' }) {
+  try {
+    if (type === 'L1') {
+      await getL1Network(provider);
+      return true;
+    } else {
+      await getL2Network(provider);
+      return true;
+    }
+  } catch (e) {
+    return false;
+  }
+}
 
 export const registerNewNetwork = async (
-  l1Provider: JsonRpcProvider,
-  l2Provider: JsonRpcProvider,
+  parentProvider: JsonRpcProvider,
+  childProvider: JsonRpcProvider,
   rollupAddress: string,
 ): Promise<{
-  l1Network: L1Network;
+  l1Network: L1Network | Omit<L2Network, 'tokenBridge'>;
   l2Network: Omit<L2Network, 'tokenBridge'>;
 }> => {
-  const l1NetworkInfo = await l1Provider.getNetwork();
-  const l2NetworkInfo = await l2Provider.getNetwork();
+  const parentNetworkInfo = await parentProvider.getNetwork();
+  const childNetworkInfo = await childProvider.getNetwork();
 
-  const l1Network: L1Network = {
-    blockTime: 10,
-    chainID: l1NetworkInfo.chainId,
-    explorerUrl: '',
-    isCustom: true,
-    name: l1NetworkInfo.name,
-    partnerChainIDs: [l2NetworkInfo.chainId],
-    isArbitrum: false,
-  };
-
-  const rollup = RollupAdminLogic__factory.connect(rollupAddress, l1Provider);
-  const l2Network: L2Network = {
-    chainID: l2NetworkInfo.chainId,
+  const rollup = RollupAdminLogic__factory.connect(rollupAddress, parentProvider);
+  const childNetwork: L2Network = {
+    chainID: childNetworkInfo.chainId,
     confirmPeriodBlocks: (await rollup.confirmPeriodBlocks()).toNumber(),
     ethBridge: {
       bridge: await rollup.bridge(),
@@ -49,7 +49,7 @@ export const registerNewNetwork = async (
     isArbitrum: true,
     isCustom: true,
     name: 'OrbitChain',
-    partnerChainID: l1NetworkInfo.chainId,
+    partnerChainID: parentNetworkInfo.chainId,
     partnerChainIDs: [],
     retryableLifetimeSeconds: 7 * 24 * 60 * 60,
     nitroGenesisBlock: 0,
@@ -74,38 +74,75 @@ export const registerNewNetwork = async (
     blockTime: arbitrumSdkConstants.ARB_MINIMUM_BLOCK_TIME_IN_SECONDS,
   };
 
-  let isL1NetworkRegistered = false;
-  try {
-    await getL1Network(l1Provider);
-    isL1NetworkRegistered = true;
-  } catch {
-    isL1NetworkRegistered = false;
+  // If the parent chain is an Arbitrum chain, we need to register parent and child chain both as L2 chains
+  if (await isArbitrumChain(parentProvider)) {
+    const isParentNetworkRegistered = await isNetworkRegistered(parentProvider, { type: 'L2' });
+    const parentNetwork: Omit<L2Network, 'tokenBridge'> = {
+      chainID: parentNetworkInfo.chainId,
+      name: parentNetworkInfo.name,
+      confirmPeriodBlocks: (await rollup.confirmPeriodBlocks()).toNumber(),
+      ethBridge: {
+        bridge: await rollup.bridge(),
+        inbox: await rollup.inbox(),
+        outbox: await rollup.outbox(),
+        rollup: rollup.address,
+        sequencerInbox: await rollup.sequencerInbox(),
+      },
+      explorerUrl: '',
+      isArbitrum: true,
+      isCustom: true,
+      partnerChainIDs: [childNetworkInfo.chainId],
+      retryableLifetimeSeconds: 7 * 24 * 60 * 60,
+      partnerChainID: 0,
+      nitroGenesisBlock: 0,
+      nitroGenesisL1Block: 0,
+      depositTimeout: 1800000,
+      blockTime: arbitrumSdkConstants.ARB_MINIMUM_BLOCK_TIME_IN_SECONDS,
+    };
+
+    if (!isParentNetworkRegistered) {
+      addCustomNetwork({ customL2Network: parentNetwork as L2Network });
+    }
+
+    const isChildNetworkRegistered = await isNetworkRegistered(childProvider, { type: 'L2' });
+    if (!isChildNetworkRegistered) {
+      addCustomNetwork({ customL2Network: childNetwork });
+    }
+    return {
+      l1Network: parentNetwork,
+      l2Network: childNetwork,
+    };
   }
 
-  let isL2NetworkRegistered = false;
-  try {
-    await getL2Network(l2Provider);
-    isL2NetworkRegistered = true;
-  } catch {
-    isL2NetworkRegistered = false;
-  }
+  const isParentNetworkRegistered = await isNetworkRegistered(parentProvider, { type: 'L1' });
+  const isChildNetworkRegistered = await isNetworkRegistered(childProvider, { type: 'L2' });
+
+  const parentNetwork: L1Network = {
+    blockTime: 10,
+    chainID: parentNetworkInfo.chainId,
+    explorerUrl: '',
+    isCustom: true,
+    name: parentNetworkInfo.name,
+    partnerChainIDs: [childNetworkInfo.chainId],
+    isArbitrum: false,
+  };
 
   // both networks are already registered, do nothing
-  if (isL1NetworkRegistered && isL2NetworkRegistered) {
-    return { l1Network, l2Network };
+  if (isParentNetworkRegistered && isChildNetworkRegistered) {
+    return { l1Network: parentNetwork, l2Network: childNetwork };
   }
 
-  if (!isL1NetworkRegistered) {
+  if (!isParentNetworkRegistered) {
     // we can assume if parent is not registered, that means the child isn't either, because of this check:
     // https://github.com/OffchainLabs/arbitrum-sdk/blob/main/src/lib/dataEntities/networks.ts#L519
-    addCustomNetwork({ customL1Network: l1Network, customL2Network: l2Network });
-  } else if (!isL2NetworkRegistered) {
+    addCustomNetwork({ customL1Network: parentNetwork, customL2Network: childNetwork });
+  } else if (!isChildNetworkRegistered) {
     // parent is already registered, only register the child
-    addCustomNetwork({ customL2Network: l2Network });
+    addCustomNetwork({ customL2Network: childNetwork });
   }
 
   return {
-    l1Network,
-    l2Network,
+    l1Network: parentNetwork,
+    l2Network: childNetwork,
   };
 };
