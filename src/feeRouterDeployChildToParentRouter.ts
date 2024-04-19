2c4a03115e693878f3a0cf81d469088f7a2288ee
TucksonDev <tucksondev@gmail.com>
2024-04-19 09:57:06 +0100

    Add JSDocs

--- src/feeRouterDeployChildToParentRouter.ts
+++ src/feeRouterDeployChildToParentRouter.ts
@@ -31,14 +31,38 @@ const oneAddress = getAddress(
 );
 
 /**
- *
+ * Deploys the ChildToParentRouter smart contract and initializes it with the provided configuration.
+ * 
+ * If the router is intended to route the native asset, there's no need to include the rollup and parentChainTokenAddress parameters.
+ * 
+ * References:
+ * - ChildToParentRouter contract: https://github.com/OffchainLabs/fund-distribution-contracts/blob/main/src/FeeRouter/ChildToParentRewardRouter.sol
+ * 
+ * Example: [Setup fee routing for the AEP](https://github.com/OffchainLabs/arbitrum-orbit-sdk/blob/main/examples/setup-aep-fee-router/index.ts)
+ * 
  * @param {FeeRouterDeployChildToParentRouterParams} feeRouterDeployChildToParentRouterParams {@link FeeRouterDeployChildToParentRouterParams}
  * @param {PublicClient} feeRouterDeployChildToParentRouterParams.parentChainPublicClient - The parent chain Viem public client
  * @param {WalletClient} feeRouterDeployChildToParentRouterParams.orbitChainWalletClient - The orbit chain Viem wallet client (this account will deploy the contract)
  * @param {Address} feeRouterDeployChildToParentRouterParams.parentChainTargetAddress - The address where funds will be sent in the parent chain
  * @param {bigint} feeRouterDeployChildToParentRouterParams.minDistributionInvervalSeconds - [Optional] The number of seconds that needs to pass before funds can be sent again (to prevent griefing)
- * @param {Address} feeRouterDeployChildToParentRouterParams.rollup - The Rollup contract address of the chain
+ * @param {Address} feeRouterDeployChildToParentRouterParams.rollup - [Optional] If sending a token different than the native token of the Orbit chain, the Rollup contract address of the chain
  * @param {Address} feeRouterDeployChildToParentRouterParams.parentChainTokenAddress - [Optional] If sending a token different than the native token of the Orbit chain, address of the token in the parent chain
+ * 
+ * @returns Promise<0x${string}> - The hash of the deployment transaction
+ * 
+ * @example
+ * const childToParentRouterDeploymentTransactionHash = await feeRouterDeployChildToParentRouter({
+ *   parentChainPublicClient,
+ *   orbitChainWalletClient,
+ *   parentChainTargetAddress,
+ * });
+ * const childToParentRouterDeploymentTransactionReceipt =
+ *   await nitroTestnodeL2Client.waitForTransactionReceipt({
+ *     hash: childToParentRouterDeploymentTransactionHash,
+ *   });
+ * const childToParentRouterAddress = getAddress(
+ *   childToParentRouterDeploymentTransactionReceipt.contractAddress as `0x${string}`,
+ * );
  */
 export async function feeRouterDeployChildToParentRouter({
   parentChainPublicClient,
--- src/feeRouterDeployRewardDistributor.ts
+++ src/feeRouterDeployRewardDistributor.ts
@@ -19,13 +19,43 @@ export type FeeRouterDeployRewardDistributorParams = {
 };
 
 /**
- *
+ * Deploys the RewardDistributor smart contract and initializes it with the provided configuration.
+ * 
+ * References:
+ * - ChildToParentRouter contract: https://github.com/OffchainLabs/fund-distribution-contracts/blob/main/src/RewardDistributor.sol
+ * 
+ * Example: [Setup fee routing for the AEP](https://github.com/OffchainLabs/arbitrum-orbit-sdk/blob/main/examples/setup-aep-fee-router/index.ts)
+ * 
  * @param {FeeRouterDeployRewardDistributorParams} feeRouterDeployRewardDistributorParams {@link FeeRouterDeployRewardDistributorParams}
  * @param {WalletClient} feeRouterDeployRewardDistributorParams.orbitChainWalletClient - The orbit chain Viem wallet client (this account will deploy the contract)
  * @param {RewardDistributorRecipient[]} feeRouterDeployRewardDistributorParams.recipients - The recipients of the rewards (array objects with properties "account" and "weight") {@link RewardDistributorRecipient}
  * @param {Address} feeRouterDeployRewardDistributorParams.recipients.account - Recipient of rewards
  * @param {Address} feeRouterDeployRewardDistributorParams.recipients.weight - Percentage of the reward obtained multiplied by 100 (e.g., for obtaining 25%, the weight would be 2500)
  *
+ * @returns Promise<0x${string}> - The hash of the deployment transaction
+ * 
+ * @example
+ * const recipients = [
+ *   {
+ *     account: randomAccount.address,
+ *     weight: 9000n,
+ *   },
+ *   {
+ *     account: randomAccount2.address,
+ *     weight: 1000n,
+ *   },
+ * ];
+ * const rewardDistributorDeploymentTransactionHash = await feeRouterDeployRewardDistributor({
+ *   orbitChainWalletClient,
+ *   recipients,
+ * });
+ * const rewardDistributorDeploymentTransactionReceipt =
+ *   await nitroTestnodeL2Client.waitForTransactionReceipt({
+ *     hash: rewardDistributorDeploymentTransactionHash,
+ *   });
+ * const rewardDistributorAddress = getAddress(
+ *   rewardDistributorDeploymentTransactionReceipt.contractAddress as `0x${string}`,
+ * );
  */
 export async function feeRouterDeployRewardDistributor({
   orbitChainWalletClient,
