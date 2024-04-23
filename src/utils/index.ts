27ca21ebb1a5bb9ac65f5938bd3285ef9175858c
spsjvc <git@spsjvc.com>
2024-04-23 20:43:23 +0200

    feat: add util for fetching client version (#84)

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,5 +2,12 @@ import { generateChainId } from './generateChainId';
 import { getParentChainLayer } from './getParentChainLayer';
 import { sanitizePrivateKey } from './sanitizePrivateKey';
 import { getArbOSVersion } from './getArbOSVersion';
+import { getClientVersion } from './getClientVersion';
 
-export { generateChainId, getParentChainLayer, sanitizePrivateKey, getArbOSVersion };
+export {
+  generateChainId,
+  getParentChainLayer,
+  sanitizePrivateKey,
+  getArbOSVersion,
+  getClientVersion,
+};
