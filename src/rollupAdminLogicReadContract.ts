699739024e36826df1f2ea32da8c38f11b57895c
Christophe <contact@christophedeveaux.com>
2024-05-06 21:40:04 +0000

    Update rollupAdminLogic type

--- src/rollupAdminLogicReadContract.ts
+++ src/rollupAdminLogicReadContract.ts
@@ -1,30 +1,33 @@
 import {
   Address,
   Chain,
-  GetFunctionArgs,
+  ContractFunctionArgs,
   PublicClient,
   ReadContractParameters,
   ReadContractReturnType,
   Transport,
 } from 'viem';
-
-import { GetFunctionName } from './types/utils';
 import { RollupAdminLogic__factory } from '@arbitrum/sdk/dist/lib/abi/factories/RollupAdminLogic__factory';
+
+import { GetReadContractFunctionName } from './types/utils';
 import { rollupAdminLogicABI } from './abi/rollupAdminLogicABI';
 
 export type RollupAdminLogicAbi = typeof rollupAdminLogicABI;
-export type RollupAdminLogicFunctionName = GetFunctionName<RollupAdminLogicAbi>;
+export type RollupAdminLogicFunctionName = GetReadContractFunctionName<RollupAdminLogicAbi>
+
+type RollupAdminLogicContractArgs<TFunctionName extends RollupAdminLogicFunctionName> =
+  ContractFunctionArgs<RollupAdminLogicAbi, 'pure' | 'view', TFunctionName>;
 
 export type RollupAdminLogicReadContractParameters<
   TFunctionName extends RollupAdminLogicFunctionName,
-> = {
-  functionName: TFunctionName;
+  TArgs extends RollupAdminLogicContractArgs<TFunctionName> = RollupAdminLogicContractArgs<TFunctionName>,
+> = Omit<ReadContractParameters<RollupAdminLogicAbi, TFunctionName, TArgs>, 'abi' | 'address'> & {
   rollup: Address;
-} & GetFunctionArgs<RollupAdminLogicAbi, TFunctionName>;
-
+};
 export type RollupAdminLogicReadContractReturnType<
   TFunctionName extends RollupAdminLogicFunctionName,
-> = ReadContractReturnType<RollupAdminLogicAbi, TFunctionName>;
+  TArgs extends RollupAdminLogicContractArgs<TFunctionName> = RollupAdminLogicContractArgs<TFunctionName>,
+> = ReadContractReturnType<RollupAdminLogicAbi, TFunctionName, TArgs>;
 
 export function rollupAdminLogicReadContract<
   TChain extends Chain | undefined,
