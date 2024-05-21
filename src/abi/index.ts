281ac6fa09f2516c031d53c68662dbf87aa8526c
Christophe <contact@christophedeveaux.com>
2024-05-21 12:23:42 +0000

    Remove generated ABI in abi barrel file

--- src/abi/index.ts
+++ src/abi/index.ts
@@ -1,23 +1,4 @@
 import { rollupAdminLogicABI } from './rollupAdminLogicABI';
 import { sequencerInboxABI } from './sequencerInboxABI';
-import {
-  erc20ABI,
-  arbOwnerABI,
-  arbOwnerPublicABI,
-  rollupCreatorABI,
-  tokenBridgeCreatorABI,
-  arbGasInfoABI,
-  arbAggregatorABI,
-} from '../generated';
 
-export {
-  rollupAdminLogicABI,
-  sequencerInboxABI,
-  erc20ABI,
-  arbOwnerABI,
-  arbOwnerPublicABI,
-  rollupCreatorABI,
-  tokenBridgeCreatorABI,
-  arbGasInfoABI,
-  arbAggregatorABI,
-};
+export { rollupAdminLogicABI, sequencerInboxABI };
