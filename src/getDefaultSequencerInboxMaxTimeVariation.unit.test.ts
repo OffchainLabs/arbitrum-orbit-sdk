b560ff82f35e701c811e0384178f11f23153e95a
spsjvc <git@spsjvc.com>
2024-05-24 12:27:16 +0200

    snapshot tests

--- src/getDefaultSequencerInboxMaxTimeVariation.unit.test.ts
+++ src/getDefaultSequencerInboxMaxTimeVariation.unit.test.ts
@@ -2,9 +2,9 @@ import { it, expect } from 'vitest';
 
 import { chains } from './chains';
 import { ParentChainId } from './types/ParentChain';
-import { getDefaultConfirmPeriodBlocks } from './getDefaultConfirmPeriodBlocks';
+import { getDefaultSequencerInboxMaxTimeVariation } from './getDefaultSequencerInboxMaxTimeVariation';
 
-it('returns default value for confirmPeriodBlocks', () => {
+it('returns default value for sequencerInboxMaxTimeVariation', () => {
   expect(
     chains
       .filter((chain) => chain.id !== 333333)
@@ -12,7 +12,7 @@ it('returns default value for confirmPeriodBlocks', () => {
         return {
           ...acc,
           // it's ok to cast as we've filtered out 333333 above
-          [value.id]: getDefaultConfirmPeriodBlocks(value.id as ParentChainId),
+          [value.id]: getDefaultSequencerInboxMaxTimeVariation(value.id as ParentChainId),
         };
       }, {}),
   ).toMatchSnapshot();
