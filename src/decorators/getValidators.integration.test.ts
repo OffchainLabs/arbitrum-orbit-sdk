a6b1cdbbfedd3c39031b60ade2431150b445351a
Christophe <contact@christophedeveaux.com>
2024-05-28 11:36:12 +0000

    Add integration test

---
+++ src/decorators/getValidators.integration.test.ts
@@ -0,0 +1,67 @@
+import { describe, it, expect } from 'vitest';
+import { Address, createPublicClient, http } from 'viem';
+import { generatePrivateKey, privateKeyToAccount } from 'viem/accounts';
+
+import { nitroTestnodeL2 } from '../chains';
+import { rollupAdminLogicPublicActions } from './rollupAdminLogicPublicActions';
+import { getInformationFromTestnode, getNitroTestnodePrivateKeyAccounts } from '../testHelpers';
+import { getValidators } from '../getValidators';
+
+const { l3RollupOwner } = getNitroTestnodePrivateKeyAccounts();
+const { l3Rollup, l3UpgradeExecutor } = getInformationFromTestnode();
+
+const client = createPublicClient({
+  chain: nitroTestnodeL2,
+  transport: http(),
+}).extend(
+  rollupAdminLogicPublicActions({
+    rollup: l3Rollup,
+  }),
+);
+
+async function toggleValidator(validator: Address, state: boolean) {
+  const tx = await client.rollupAdminLogicPrepareTransactionRequest({
+    functionName: 'setValidator',
+    args: [[validator], [state]],
+    account: l3RollupOwner.address,
+    upgradeExecutor: l3UpgradeExecutor,
+    rollup: l3Rollup,
+  });
+
+  await client.sendRawTransaction({
+    serializedTransaction: await l3RollupOwner.signTransaction(tx),
+  });
+}
+
+describe('successfully get validators', async () => {
+  it('when disabling the same validator multiple time', async () => {
+    const randomAccount = privateKeyToAccount(generatePrivateKey()).address;
+
+    const currentValidators = await getValidators(client, { rollupAddress: l3Rollup });
+    expect(currentValidators).toEqual([]);
+
+    await toggleValidator(randomAccount, false);
+    await toggleValidator(randomAccount, false);
+    expect(await getValidators(client, { rollupAddress: l3Rollup })).toEqual([]);
+
+    await toggleValidator(randomAccount, true);
+    expect(await getValidators(client, { rollupAddress: l3Rollup })).toEqual([randomAccount]);
+
+    // Reset state for future tests
+    await toggleValidator(randomAccount, false);
+  });
+
+  it('when enabling the same validators multiple time', async () => {
+    const randomAccount = privateKeyToAccount(generatePrivateKey()).address;
+
+    const currentValidators = await getValidators(client, { rollupAddress: l3Rollup });
+    expect(currentValidators).toEqual([]);
+
+    await toggleValidator(randomAccount, true);
+    await toggleValidator(randomAccount, true);
+    expect(await getValidators(client, { rollupAddress: l3Rollup })).toEqual([randomAccount]);
+
+    await toggleValidator(randomAccount, false);
+    expect(await getValidators(client, { rollupAddress: l3Rollup })).toEqual([]);
+  });
+});
