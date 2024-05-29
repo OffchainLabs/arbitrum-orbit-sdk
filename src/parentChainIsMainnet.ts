7426a7659b2ad392f5e8108361d419f50e3ceb01
spsjvc <git@spsjvc.com>
2024-05-29 13:42:19 +0200

    add helper

---
+++ src/parentChainIsMainnet.ts
@@ -0,0 +1,19 @@
+import * as chains from './chains';
+import { ParentChainId } from './types/ParentChain';
+
+export function parentChainIsMainnet(parentChainId: ParentChainId): boolean {
+  // doing switch here to make sure it's exhaustive when checking against `ParentChainId`
+  switch (parentChainId) {
+    case chains.mainnet.id:
+    case chains.arbitrumOne.id:
+    case chains.arbitrumNova.id:
+      return true;
+
+    case chains.sepolia.id:
+    case chains.holesky.id:
+    case chains.arbitrumSepolia.id:
+    case chains.nitroTestnodeL1.id:
+    case chains.nitroTestnodeL2.id:
+      return false;
+  }
+}
