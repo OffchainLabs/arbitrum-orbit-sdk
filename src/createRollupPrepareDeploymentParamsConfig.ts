27c24d61cdc7e62a81af29bd04f39d5a3549ecb3
spsjvc <git@spsjvc.com>
2024-05-30 16:10:11 +0200

    fix: use generics for rollup deployment config (#106)

--- src/createRollupPrepareDeploymentParamsConfig.ts
+++ src/createRollupPrepareDeploymentParamsConfig.ts
@@ -1,4 +1,4 @@
-import { PublicClient } from 'viem';
+import { Chain, Client, Transport } from 'viem';
 
 import { ChainConfig } from './types/ChainConfig';
 import { validateParentChain } from './types/ParentChain';
@@ -63,8 +63,11 @@ export type CreateRollupPrepareDeploymentParamsConfigParams = Prettify<
  *   }),
  * });
  */
-export function createRollupPrepareDeploymentParamsConfig(
-  client: PublicClient,
+export function createRollupPrepareDeploymentParamsConfig<
+  TTransport extends Transport = Transport,
+  TChain extends Chain | undefined = Chain | undefined,
+>(
+  client: Client<TTransport, TChain>,
   { chainConfig, ...params }: CreateRollupPrepareDeploymentParamsConfigParams,
 ): CreateRollupPrepareDeploymentParamsConfigResult {
   const parentChainId = validateParentChain(client);
