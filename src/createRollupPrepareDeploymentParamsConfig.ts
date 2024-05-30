14a272a354103fb71630f23fa56e1fd234dd2737
Dragiša Spasojević <dragisaspasojevic@gmail.com>
2024-05-30 10:14:01 +0200

    add jsdoc

--- src/createRollupPrepareDeploymentParamsConfig.ts
+++ src/createRollupPrepareDeploymentParamsConfig.ts
@@ -4,6 +4,7 @@ import { ChainConfig } from './types/ChainConfig';
 import { validateParentChain } from './types/ParentChain';
 import { Prettify } from './types/utils';
 
+import { createRollup } from './createRollup';
 import { CreateRollupFunctionInputs } from './types/createRollupTypes';
 import { prepareChainConfig } from './prepareChainConfig';
 
@@ -24,6 +25,44 @@ export type CreateRollupPrepareDeploymentParamsConfigParams = Prettify<
   RequiredParams & { chainConfig?: ChainConfig } & OptionalParams
 >;
 
+/**
+ * Creates the configuration object to be used with {@link createRollup}.
+ *
+ * @param {PublicClient} client Parent chain client
+ * @param {Object} params Chain configuration parameters
+ * @see https://docs.arbitrum.io/launch-orbit-chain/how-tos/customize-deployment-configuration
+ * @see https://docs.arbitrum.io/launch-orbit-chain/reference/additional-configuration-parameters
+ * @param {string} params.owner
+ * @param {BigInt} params.chainId
+ * @param {ChainConfig} [params.chainConfig]
+ * @param {BigInt} [params.confirmPeriodBlocks]
+ * @param {BigInt} [params.extraChallengeTimeBlocks]
+ * @param {string} [params.stakeToken]
+ * @param {BigInt} [params.baseStake]
+ * @param {string} [params.wasmModuleRoot]
+ * @param {string} [params.loserStakeEscrow]
+ * @param {BigInt} [params.genesisBlockNum]
+ * @param {Object} [params.sequencerInboxMaxTimeVariation]
+ * @param {BigInt} [params.sequencerInboxMaxTimeVariation.delayBlocks]
+ * @param {BigInt} [params.sequencerInboxMaxTimeVariation.futureBlocks]
+ * @param {BigInt} [params.sequencerInboxMaxTimeVariation.delaySeconds]
+ * @param {BigInt} [params.sequencerInboxMaxTimeVariation.futureSeconds]
+ *
+ * @returns {Object} {@link CreateRollupPrepareDeploymentParamsConfigResult}
+ *
+ * @example
+ * const config = createRollupPrepareDeploymentParamsConfig(parentPublicClient, {
+ *   chainId: BigInt(chainId),
+ *   owner: deployer.address,
+ *   chainConfig: prepareChainConfig({
+ *     chainId,
+ *     arbitrum: {
+ *       InitialChainOwner: deployer.address,
+ *       DataAvailabilityCommittee: true,
+ *     },
+ *   }),
+ * });
+ */
 export function createRollupPrepareDeploymentParamsConfig(
   client: PublicClient,
   { chainConfig, ...params }: CreateRollupPrepareDeploymentParamsConfigParams,
