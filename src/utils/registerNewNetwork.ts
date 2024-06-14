12728cfb4b76b218e1c323e3a46d57e84be7bd13
Dragiša Spasojević <dragisaspasojevic@gmail.com>
2024-06-14 14:32:20 +0200

    omit token bridge

--- src/utils/registerNewNetwork.ts
+++ src/utils/registerNewNetwork.ts
@@ -20,22 +20,6 @@ export const registerNewNetwork = async (
     parentChainId,
     confirmPeriodBlocks,
     ethBridge,
-    tokenBridge: {
-      l1CustomGateway: '',
-      l1ERC20Gateway: '',
-      l1GatewayRouter: '',
-      l1MultiCall: '',
-      l1ProxyAdmin: '',
-      l1Weth: '',
-      l1WethGateway: '',
-      l2CustomGateway: '',
-      l2ERC20Gateway: '',
-      l2GatewayRouter: '',
-      l2Multicall: '',
-      l2ProxyAdmin: '',
-      l2Weth: '',
-      l2WethGateway: '',
-    },
     isCustom: true,
   };
 
