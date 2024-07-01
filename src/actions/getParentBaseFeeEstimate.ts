de9b0b577f4c3ff325725c0dffa3989cac0c8d34
Christophe <contact@christophedeveaux.com>
2024-07-01 11:39:21 +0000

    Use proper case for getParentBaseFeeEstimate

--- src/actions/getParentBaseFeeEstimate.ts
+++ src/actions/getParentBaseFeeEstimate.ts
@@ -2,16 +2,16 @@ import { Chain, PublicClient, ReadContractReturnType, Transport } from 'viem';
 import { arbGasInfo } from '../contracts';
 
 type ArbGasInfoABI = typeof arbGasInfo.abi;
-export type GetParentbaseFeeEstimateParameters = void;
+export type GetParentBaseFeeEstimateParameters = void;
 
-export type GetParentbaseFeeEstimateReturnType = ReadContractReturnType<
+export type GetParentBaseFeeEstimateReturnType = ReadContractReturnType<
   ArbGasInfoABI,
   'getL1BaseFeeEstimate'
 >;
 
-export async function getParentbaseFeeEstimate<TChain extends Chain | undefined>(
+export async function getParentBaseFeeEstimate<TChain extends Chain | undefined>(
   client: PublicClient<Transport, TChain>,
-): Promise<GetParentbaseFeeEstimateReturnType> {
+): Promise<GetParentBaseFeeEstimateReturnType> {
   return client.readContract({
     abi: arbGasInfo.abi,
     functionName: 'getL1BaseFeeEstimate',
