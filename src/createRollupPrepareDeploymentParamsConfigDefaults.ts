d06f0ca7bb5eae8b678d63eb7f1d04076cc17ad2
Dragiša Spasojević <dragisaspasojevic@gmail.com>
2024-08-13 14:26:13 +0200

    feat: use consensus v31 wasm module root

--- src/createRollupPrepareDeploymentParamsConfigDefaults.ts
+++ src/createRollupPrepareDeploymentParamsConfigDefaults.ts
@@ -6,7 +6,7 @@ export const defaults = {
   extraChallengeTimeBlocks: BigInt(0),
   stakeToken: zeroAddress,
   baseStake: parseEther(String(0.1)),
-  wasmModuleRoot: getWasmModuleRoot(20),
+  wasmModuleRoot: getWasmModuleRoot(31),
   loserStakeEscrow: zeroAddress,
   genesisBlockNum: BigInt(0),
 } as const;
