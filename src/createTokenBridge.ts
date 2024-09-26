8cd7791ce2eb3e03ddab387dd7979c841163f0cf
Christophe Deveaux <contact@christophedeveaux.com>
2024-09-26 16:33:52 +0200

    fix: check if token bridge is already deployed (#186)

--- src/createTokenBridge.ts
+++ src/createTokenBridge.ts
@@ -34,6 +34,58 @@ import { isCustomFeeTokenAddress } from './utils/isCustomFeeTokenAddress';
 import { WithTokenBridgeCreatorAddressOverride } from './types/createTokenBridgeTypes';
 import { TransactionRequestGasOverrides } from './utils/gasOverrides';
 import { getBlockExplorerUrl } from './utils/getBlockExplorerUrl';
+import { tokenBridgeCreatorABI } from './contracts/TokenBridgeCreator';
+import { getTokenBridgeCreatorAddress } from './utils';
+import { rollupABI } from './contracts/Rollup';
+
+/**
+ * If token bridge was already deployed, `createTokenBridge` will fail when waiting for retryables.
+ * This function returns true if token bridge was deployed previously.
+ *
+ * @param {String} assertTokenBridgeDoesntExistParams.parentChainPublicClient - The parent chain Viem Public Client
+ * @param {String} assertTokenBridgeDoesntExistParams.orbitChainPublicClient - The orbit chain Viem Public Client
+ * @param {String=} assertTokenBridgeDoesntExistParams.tokenBridgeCreatorAddress - The TokenBridgeCreator address.
+ * Default to getTokenBridgeCreatorAddress(parentChainPublicClient) if not provided
+ * @param {String} assertTokenBridgeDoesntExistParams.rollupAddress - The address of the rollup on the parent chain
+ *
+ * @returns true if token bridge was already deployed
+ */
+export async function isTokenBridgeDeployed<
+  TParentChain extends Chain | undefined,
+  TOrbitChain extends Chain | undefined,
+>({
+  parentChainPublicClient,
+  orbitChainPublicClient,
+  tokenBridgeCreatorAddress,
+  rollupAddress,
+}: {
+  parentChainPublicClient: PublicClient<Transport, TParentChain>;
+  orbitChainPublicClient: PublicClient<Transport, TOrbitChain>;
+  tokenBridgeCreatorAddress?: Address;
+  rollupAddress: Address;
+}) {
+  const inbox = await parentChainPublicClient.readContract({
+    address: rollupAddress,
+    abi: rollupABI,
+    functionName: 'inbox',
+  });
+
+  const [router] = await parentChainPublicClient.readContract({
+    address: tokenBridgeCreatorAddress ?? getTokenBridgeCreatorAddress(parentChainPublicClient),
+    abi: tokenBridgeCreatorABI,
+    functionName: 'inboxToL2Deployment',
+    args: [inbox],
+  });
+
+  if (router) {
+    const code = await orbitChainPublicClient.getBytecode({ address: router });
+    if (code) {
+      return true;
+    }
+  }
+
+  return false;
+}
 
 export type CreateTokenBridgeParams<
   TParentChain extends Chain | undefined,
@@ -171,6 +223,17 @@ export async function createTokenBridge<
 }: CreateTokenBridgeParams<TParentChain, TOrbitChain>): Promise<
   CreateTokenBridgeResults<TParentChain, TOrbitChain>
 > {
+  const isTokenBridgeAlreadyDeployed = await isTokenBridgeDeployed({
+    parentChainPublicClient,
+    orbitChainPublicClient,
+    tokenBridgeCreatorAddress: tokenBridgeCreatorAddressOverride,
+    rollupAddress,
+  });
+
+  if (isTokenBridgeAlreadyDeployed) {
+    throw new Error(`Token bridge contracts for Rollup ${rollupAddress} are already deployed`);
+  }
+
   const isCustomFeeTokenBridge = isCustomFeeTokenAddress(nativeTokenAddress);
   if (isCustomFeeTokenBridge) {
     // set the custom fee token
