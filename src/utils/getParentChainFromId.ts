f403266ceadb8cdf529c6dc6ca0ebfcec12a5c61
spsjvc <git@spsjvc.com>
2024-10-09 18:32:05 +0200

    fix

--- src/utils/getParentChainFromId.ts
+++ src/utils/getParentChainFromId.ts
@@ -1,14 +1,14 @@
-import { extractChain } from 'viem';
+import { Chain, extractChain } from 'viem';
 
-import { ParentChain, validateParentChain } from '../types/ParentChain';
+import { validateParentChain } from '../types/ParentChain';
 import { chains } from '../chains';
 import { customChains } from '../customChains';
 
-export function getParentChainFromId(chainId: number): ParentChain {
+export function getParentChainFromId(chainId: number): Chain {
   const { chainId: parentChainId } = validateParentChain(chainId);
 
   return extractChain({
     chains: [...chains, ...customChains],
     id: parentChainId,
-  }) as ParentChain;
+  });
 }
