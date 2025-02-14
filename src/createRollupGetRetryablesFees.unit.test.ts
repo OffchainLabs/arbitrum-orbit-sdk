415968f62e93bad247fc9c489b271dc50a4bf1b5
spsjvc <git@spsjvc.com>
2025-02-14 13:33:03 +0100

    test: use different sepolia rpc (#237)

--- src/createRollupGetRetryablesFees.unit.test.ts
+++ src/createRollupGetRetryablesFees.unit.test.ts
@@ -6,7 +6,7 @@ import { createRollupGetRetryablesFees } from './createRollupGetRetryablesFees';
 
 const sepoliaClient = createPublicClient({
   chain: sepolia,
-  transport: http(),
+  transport: http('https://gateway.tenderly.co/public/sepolia'),
 });
 
 it('successfully fetches retryable fees for an eth-based chain', async () => {
