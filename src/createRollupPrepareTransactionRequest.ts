0b044de4787799d972b847851ad32a5f10b9bf88
spsjvc <git@spsjvc.com>
2025-03-21 13:00:04 +0100

    revert

--- src/createRollupPrepareTransactionRequest.ts
+++ src/createRollupPrepareTransactionRequest.ts
@@ -109,12 +109,12 @@ export async function createRollupPrepareTransactionRequest<TChain extends Chain
   const batchPosterManager = params.batchPosterManager ?? zeroAddress;
   const paramsWithDefaults = { ...defaults, ...params, maxDataSize, batchPosterManager };
   const createRollupGetCallValueParams = { ...paramsWithDefaults, account };
-  const data = createRollupEncodeFunctionData([paramsWithDefaults]);
 
+  // @ts-ignore (todo: fix viem type issue)
   const request = await publicClient.prepareTransactionRequest({
     chain: publicClient.chain,
     to: rollupCreatorAddressOverride ?? getRollupCreatorAddress(publicClient),
-    data,
+    data: createRollupEncodeFunctionData([paramsWithDefaults]),
     value: value ?? (await createRollupGetCallValue(publicClient, createRollupGetCallValueParams)),
     account,
     // if the base gas limit override was provided, hardcode gas to 0 to skip estimation
