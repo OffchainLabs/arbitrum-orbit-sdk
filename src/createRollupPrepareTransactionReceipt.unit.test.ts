c545deaf08a89e70dc0792d35d83beadd6d7646f
spsjvc <git@spsjvc.com>
2025-03-24 17:10:24 +0100

    add v3.0 test case

--- src/createRollupPrepareTransactionReceipt.unit.test.ts
+++ src/createRollupPrepareTransactionReceipt.unit.test.ts
@@ -24,3 +24,11 @@ it('successfully parses core contracts from a tx receipt on RollupCreator v2.1',
   });
   expect(createRollupPrepareTransactionReceipt(txReceipt).getCoreContracts()).toMatchSnapshot();
 });
+
+// https://sepolia.arbiscan.io/tx/0xda485d8ab8d47a8e9ea46677df8790b94faf3888fb1afbc52bb7bc104c29c3f3
+it('successfully parses core contracts from a tx receipt on RollupCreator v3.0', async () => {
+  const txReceipt = await client.getTransactionReceipt({
+    hash: '0xda485d8ab8d47a8e9ea46677df8790b94faf3888fb1afbc52bb7bc104c29c3f3',
+  });
+  expect(createRollupPrepareTransactionReceipt(txReceipt).getCoreContracts()).toMatchSnapshot();
+});
