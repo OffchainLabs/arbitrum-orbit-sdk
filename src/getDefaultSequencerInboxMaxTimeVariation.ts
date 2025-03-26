5a19bf7f5d5ed7c234ace02a18d74ae751300b9b
spsjvc <git@spsjvc.com>
2025-03-26 17:25:38 +0100

    refactor

--- src/getDefaultSequencerInboxMaxTimeVariation.ts
+++ src/getDefaultSequencerInboxMaxTimeVariation.ts
@@ -1,7 +1,7 @@
 import { Client, Transport, Chain } from 'viem';
 
 import { ParentChainId, validateParentChain } from './types/ParentChain';
-import { base, baseSepolia } from './chains';
+import { getParentChainBlockTime } from './getParentChainBlockTime';
 
 export type SequencerInboxMaxTimeVariation = {
   delayBlocks: bigint;
@@ -13,25 +13,23 @@ export type SequencerInboxMaxTimeVariation = {
 export function getDefaultSequencerInboxMaxTimeVariation<TChain extends Chain | undefined>(
   parentChainIdOrClient: ParentChainId | Client<Transport, TChain>,
 ): SequencerInboxMaxTimeVariation {
-  const { chainId: parentChainId } = validateParentChain(parentChainIdOrClient);
+  const { chainId: parentChainId, isCustom: parentChainIsCustom } =
+    validateParentChain(parentChainIdOrClient);
+
+  if (parentChainIsCustom) {
+    throw new Error(
+      `[getDefaultSequencerInboxMaxTimeVariation] can't provide defaults for custom parent chain with id ${parentChainId}`,
+    );
+  }
 
   const delaySeconds = 60 * 60 * 24 * 4; // 4 days;
   const futureSeconds = 60 * 60; // 1 hour;
 
-  if (parentChainId === base.id || parentChainId === baseSepolia.id) {
-    // For Arbitrum L3s built on top of an OP Stack L2, `block.number` will return the L2 block number.
-    // L2 blocks in OP Stack chains are produced every 2 seconds.
-    return {
-      delayBlocks: BigInt(delaySeconds / 2),
-      futureBlocks: BigInt(futureSeconds / 2),
-      delaySeconds: BigInt(delaySeconds),
-      futureSeconds: BigInt(futureSeconds),
-    };
-  }
+  const blockTime = getParentChainBlockTime(parentChainId);
 
   return {
-    delayBlocks: BigInt(delaySeconds / 12),
-    futureBlocks: BigInt(futureSeconds / 12),
+    delayBlocks: BigInt(delaySeconds / blockTime),
+    futureBlocks: BigInt(futureSeconds / blockTime),
     delaySeconds: BigInt(delaySeconds),
     futureSeconds: BigInt(futureSeconds),
   };
