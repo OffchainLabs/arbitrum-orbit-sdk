f492ca514cc3a4712d380be98dd6954632efbed5
spsjvc <git@spsjvc.com>
2025-03-27 12:30:52 +0100

    feat: update default confirm period blocks to 7 days (#250)

--- src/getDefaultConfirmPeriodBlocks.ts
+++ src/getDefaultConfirmPeriodBlocks.ts
@@ -2,7 +2,7 @@ import { Client, Transport, Chain } from 'viem';
 
 import { ParentChainId, validateParentChain } from './types/ParentChain';
 import { parentChainIsMainnet } from './parentChainIsMainnet';
-import { base, baseSepolia } from './chains';
+import { getParentChainBlockTime } from './getParentChainBlockTime';
 
 export function getDefaultConfirmPeriodBlocks<TChain extends Chain | undefined>(
   parentChainIdOrClient: ParentChainId | Client<Transport, TChain>,
@@ -17,16 +17,11 @@ export function getDefaultConfirmPeriodBlocks<TChain extends Chain | undefined>(
   }
 
   const isMainnet = parentChainIsMainnet(parentChainId);
-  const confirmPeriodBlocks = isMainnet ? 45_818n : 150n;
+  const blocksPerMinute = 60 / getParentChainBlockTime(parentChainId);
 
-  if (parentChainId === base.id || parentChainId === baseSepolia.id) {
-    // For Arbitrum L2s built on top of Ethereum, or Arbitrum L3s built on top of an Arbitrum L2, `block.number` always returns the L1 block number.
-    // L1 blocks are produced every 12 seconds.
-    //
-    // For Arbitrum L3s built on top of an OP Stack L2, `block.number` will return the L2 block number.
-    // L2 blocks in OP Stack chains are produced every 2 seconds, which is 6 times more frequent, so we have to multiply the result by 6 to achieve the same.
-    return confirmPeriodBlocks * 6n;
-  }
-
-  return confirmPeriodBlocks;
+  return isMainnet
+    ? // 7 days
+      BigInt(7 * 24 * 60 * blocksPerMinute)
+    : // 30 minutes
+      BigInt(30 * blocksPerMinute);
 }
