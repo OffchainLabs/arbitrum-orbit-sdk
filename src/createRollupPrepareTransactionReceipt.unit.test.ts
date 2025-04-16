51c03aeacd16395a57a6fd772d8d069d78505fc8
spsjvc <git@spsjvc.com>
2025-04-16 18:59:54 +0200

    bump test

--- src/createRollupPrepareTransactionReceipt.unit.test.ts
+++ src/createRollupPrepareTransactionReceipt.unit.test.ts
@@ -1,12 +1,17 @@
 import { it, expect } from 'vitest';
 import { createPublicClient, http } from 'viem';
-import { arbitrumSepolia } from 'viem/chains';
+import { sepolia, arbitrumSepolia } from 'viem/chains';
 
 import { createRollupPrepareTransactionReceipt } from './createRollupPrepareTransactionReceipt';
 
 const client = createPublicClient({
   chain: arbitrumSepolia,
   transport: http(),
+});
+
+const sepoliaClient = createPublicClient({
+  chain: sepolia,
+  transport: http('https://sepolia.gateway.tenderly.co'),
 });
 
 // https://sepolia.arbiscan.io/tx/0x5b0b49e0259289fc89949a55a5ad35a8939440a55065d29b14e5e7ef7494efff
@@ -25,10 +30,10 @@ it('successfully parses core contracts from a tx receipt on RollupCreator v2.1',
   expect(createRollupPrepareTransactionReceipt(txReceipt).getCoreContracts()).toMatchSnapshot();
 });
 
-// https://sepolia.arbiscan.io/tx/0xda485d8ab8d47a8e9ea46677df8790b94faf3888fb1afbc52bb7bc104c29c3f3
-it('successfully parses core contracts from a tx receipt on RollupCreator v3.0', async () => {
-  const txReceipt = await client.getTransactionReceipt({
-    hash: '0xda485d8ab8d47a8e9ea46677df8790b94faf3888fb1afbc52bb7bc104c29c3f3',
+// https://sepolia.etherscan.io/tx/0x751f1b2bab2806769f663db2141d434e4d8c9b65bc4a5d7ca10ed597f918191f
+it('successfully parses core contracts from a tx receipt on RollupCreator v3.1', async () => {
+  const txReceipt = await sepoliaClient.getTransactionReceipt({
+    hash: '0x751f1b2bab2806769f663db2141d434e4d8c9b65bc4a5d7ca10ed597f918191f',
   });
   expect(createRollupPrepareTransactionReceipt(txReceipt).getCoreContracts()).toMatchSnapshot();
 });
