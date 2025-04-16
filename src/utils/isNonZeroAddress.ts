3cac593af052e051856edbd8c7ee5c0bda0ce1eb
spsjvc <git@spsjvc.com>
2025-04-16 16:20:48 +0200

    pt2

--- src/utils/isNonZeroAddress.ts
+++ src/utils/isNonZeroAddress.ts
@@ -1,5 +1,5 @@
 import { Address, isAddress, zeroAddress } from 'viem';
 
-export function isValidNonZeroAddress(address: Address | undefined): address is Address {
+export function isNonZeroAddress(address: Address | undefined): address is Address {
   return typeof address !== 'undefined' && isAddress(address) && address !== zeroAddress;
 }
