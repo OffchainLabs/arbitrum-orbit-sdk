fa4a4b0f0a36cdf6588088775591677a7b9a58c0
spsjvc <git@spsjvc.com>
2025-05-21 15:13:11 +0200

    rename

--- src/prepareKeysetHash.unit.test.ts
+++ src/prepareKeysetHash.unit.test.ts
@@ -1,9 +1,10 @@
 import { expect, it } from 'vitest';
 
 import { prepareKeysetHash } from './prepareKeysetHash';
-import { logs } from './prepareKeysetHash.unit.testInputs';
 
-logs.forEach((log, index) => {
+import { logs as logs_arbitrumNova } from './prepareKeysetHash.unit.testInputs-arbitrumNova';
+
+logs_arbitrumNova.forEach((log, index) => {
   it(`successfully calculates keyset hash (example #${index + 1})`, () => {
     const { keysetBytes, keysetHash } = log.args;
     expect(prepareKeysetHash(keysetBytes)).toEqual(keysetHash);
