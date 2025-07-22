bd7d6da0abdd8472968112b0f0cd2c36117673dc
spsjvc <git@spsjvc.com>
2025-07-22 18:52:05 +0200

    feat: add versioning to getRollupCreatorAddress (#470)

--- src/utils/getRollupCreatorAddress.ts
+++ src/utils/getRollupCreatorAddress.ts
@@ -1,10 +1,12 @@
 import { Client, Transport, Chain, ChainContract, Address } from 'viem';
 
-import { rollupCreatorAddress } from '../contracts/RollupCreator';
+import { rollupCreatorAddress as rollupCreatorV3Dot1Address } from '../contracts/RollupCreator';
+import { rollupCreatorAddress as rollupCreatorV2Dot1Address } from '../contracts/RollupCreator/v2.1';
 import { validateParentChain } from '../types/ParentChain';
 
 export function getRollupCreatorAddress<TChain extends Chain | undefined>(
   client: Client<Transport, TChain>,
+  rollupCreatorVersion: 'v2.1' | 'v3.1' = 'v3.1',
 ): Address {
   const { chainId: parentChainId, isCustom: parentChainIsCustom } = validateParentChain(client);
 
@@ -20,6 +22,13 @@ export function getRollupCreatorAddress<TChain extends Chain | undefined>(
 
     return address;
   }
+
+  const rollupCreatorAddress =
+    rollupCreatorVersion === 'v3.1'
+      ? //
+        rollupCreatorV3Dot1Address
+      : //
+        rollupCreatorV2Dot1Address;
 
   return rollupCreatorAddress[parentChainId];
 }
