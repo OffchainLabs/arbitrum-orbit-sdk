f36d6bf1a45dbd1ef0f21a8917a06c6c6442083c
spsjvc <git@spsjvc.com>
2025-07-22 15:20:06 +0200

    feat: add contract versions to getRollupCreatorAddress

--- src/utils/getRollupCreatorAddress.unit.test.ts
+++ src/utils/getRollupCreatorAddress.unit.test.ts
@@ -7,13 +7,31 @@ import { registerCustomParentChain } from '../chains';
 
 import { testHelper_createCustomParentChain } from '../testHelpers';
 
-it(`successfully returns address for Sepolia`, () => {
+it(`successfully returns address for Sepolia (latest)`, () => {
   const client = createPublicClient({
     chain: sepolia,
     transport: http(),
   });
 
   expect(getRollupCreatorAddress(client)).toEqual('0x687Bc1D23390875a868Db158DA1cDC8998E31640');
+});
+
+it(`successfully returns address for Sepolia (v3.1)`, () => {
+  const client = createPublicClient({
+    chain: sepolia,
+    transport: http(),
+  });
+
+  expect(getRollupCreatorAddress(client, 'v3.1')).toEqual('0x687Bc1D23390875a868Db158DA1cDC8998E31640');
+});
+
+it(`successfully returns address for Sepolia (v2.1)`, () => {
+  const client = createPublicClient({
+    chain: sepolia,
+    transport: http(),
+  });
+
+  expect(getRollupCreatorAddress(client, 'v2.1')).toEqual('0xfb774eA8A92ae528A596c8D90CBCF1bdBC4Cee79');
 });
 
 it(`fails to return address for an unrecognized parent chain`, () => {
