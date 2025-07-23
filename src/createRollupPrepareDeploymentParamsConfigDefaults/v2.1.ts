49f8ba7e6f9e382805722809ecbb5fcada2fbe32
spsjvc <git@spsjvc.com>
2025-07-23 17:04:46 +0200

    refactor: add versioning to createRollupPrepareDeploymentParamsConfigDefaults

---
+++ src/createRollupPrepareDeploymentParamsConfigDefaults/v2.1.ts
@@ -0,0 +1,12 @@
+import { parseEther, zeroAddress } from 'viem';
+
+import { getConsensusReleaseByVersion } from '../wasmModuleRoot';
+
+export const defaults = {
+  extraChallengeTimeBlocks: BigInt(0),
+  stakeToken: zeroAddress,
+  baseStake: parseEther(String(0.1)),
+  wasmModuleRoot: getConsensusReleaseByVersion(32).wasmModuleRoot,
+  loserStakeEscrow: zeroAddress,
+  genesisBlockNum: BigInt(0),
+} as const;
