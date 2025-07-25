c7525f5c02233175d589dc82d8be0ffe909f56a1
spsjvc <git@spsjvc.com>
2025-07-25 14:02:34 +0200

    bump

--- src/prepareChainConfig.ts
+++ src/prepareChainConfig.ts
@@ -1,22 +1,6 @@
 import { ExcludeSome, Prettify, RequireSome } from './types/utils';
 import { ChainConfig, ChainConfigArbitrumParams } from './types/ChainConfig';
-
-function withSortedKeys<T extends Record<string, any>>(obj: T): T {
-  const result = {} as T;
-  const sortedKeys = Object.keys(obj).sort();
-
-  for (const key of sortedKeys) {
-    const value = obj[key];
-
-    if (value && typeof value === 'object' && !Array.isArray(value)) {
-      result[key as keyof T] = withSortedKeys(value);
-    } else {
-      result[key as keyof T] = value;
-    }
-  }
-
-  return result;
-}
+import { withSortedKeys } from './utils/withSortedKeys';
 
 export const defaults = {
   homesteadBlock: 0,
