47b12e903d6756e5ffc3bf6f482686e89ead50ac
spsjvc <git@spsjvc.com>
2025-07-26 18:18:55 +0200

    pass in rollup creator version to retryable fees

--- src/createRollupPrepareTransactionRequest.ts
+++ src/createRollupPrepareTransactionRequest.ts
@@ -138,7 +138,7 @@ export async function createRollupPrepareTransactionRequest<TChain extends Chain
   }
 
   const paramsWithDefaults = { ...defaults, ...params, maxDataSize };
-  const createRollupGetCallValueParams = { ...paramsWithDefaults, account };
+  const createRollupGetCallValueParams = { ...paramsWithDefaults, account, rollupCreatorVersion };
 
   // @ts-ignore (todo: fix viem type issue)
   const request = await publicClient.prepareTransactionRequest({
